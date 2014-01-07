a2de53d348290d0671b0cffc97873e6787e04ec6
Matt Gambogi <m@gambogi.com>
2014-01-07 12:27:18 -0500

    Removed unused code

--- CSHLDAP.py
+++ CSHLDAP.py
@@ -62,27 +62,3 @@ class CSHLDAP:
         modlist = pyldap.modlist.modifyModlist(old_attrs, new_attrs)
         
         self.ldap.modify_s(dn, modlist)
-
-class KerberosTicket:
-    def __init__(self, service):
-        __, krb_context = kerberos.authGSSClientInit(service)
-        kerberos.authGSSClientStep(krb_context, "")
-        self._krb_context = krb_context
-        self.auth_header = ("Negotiate " +
-                            kerberos.authGSSClientResponse(krb_context))
-    def verify_response(self, auth_header):
-        # Handle comma-separated lists of authentication fields
-        for field in auth_header.split(","):
-            kind, __, details = field.strip().partition(" ")
-            if kind.lower() == "negotiate":
-                auth_details = details.strip()
-                break
-        else:
-            raise ValueError("Negotiate not found in %s" % auth_header)
-        # Finish the Kerberos handshake
-        krb_context = self._krb_context
-        if krb_context is None:
-            raise RuntimeError("Ticket already used for verification")
-        self._krb_context = None
-        kerberos.authGSSClientStep(krb_context, auth_details)
-        kerberos.authGSSClientClean(krb_context)
