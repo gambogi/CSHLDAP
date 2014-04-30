5ccea4cc1741eba26d8e66620ac1d3ff15fe3bf0
Harlan Haskins <harlan@harlanhaskins.com>
2014-04-30 16:42:52 -0400

    Added Member class to CSHLDAP and added pyc to gitignore.

--- CSHLDAP.py
+++ CSHLDAP.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # written for python 2.7
 
-import ldap as pyldap       
+import ldap as pyldap
 import ldap.sasl as sasl
 import ldap.modlist
 from copy import deepcopy
@@ -21,7 +21,7 @@ class CSHLDAP:
         else:
             try:
                 auth = sasl.gssapi("")
-            	
+
                 self.ldap.sasl_interactive_bind_s("", auth)
             	self.ldap.set_option(pyldap.OPT_DEBUG_LEVEL,0)
             except pyldap.LDAPError, e:
@@ -30,7 +30,7 @@ class CSHLDAP:
 
     def members(self, uid="*"):
         """ members() issues an ldap query for all users, and returns a dict
-            for each matching entry. This can be quite slow, and takes roughly 
+            for each matching entry. This can be quite slow, and takes roughly
             3s to complete. You may optionally restrict the scope by specifying
             a uid, which is roughly equivalent to a search(uid='foo')
         """
@@ -41,15 +41,15 @@ class CSHLDAP:
         return result
 
     def member(self, user):
-        """ Returns a user as a dict of attributes 
+        """ Returns a user as a dict of attributes
         """
         return self.search(uid=user)[0][1]
 
     def eboard(self):
         """ Returns a list of eboard members formatted as a search
-            inserts an extra ['commmittee'] attribute 
+            inserts an extra ['commmittee'] attribute
         """
-        # self.committee used as base because that's where eboard 
+        # self.committee used as base because that's where eboard
         # info is kept
         committees = self.search(base = self.committees, cn='*')
         directors = []
@@ -74,7 +74,7 @@ class CSHLDAP:
     def getGroups(self, member_dn):
         searchResult = self.search(base=self.groups, member=member_dn)
         if len(searchResult) == 0: return []
-        
+
         groupList = []
         for group in searchResult:
             groupList.append(group[1]['cn'][0])
@@ -93,28 +93,28 @@ class CSHLDAP:
     def trimResult(self, result):
         return [x[1] for x in result]
 
-    def search( self, base=False, trim=False, **kwargs ):
+    def search( self, base=False, trim=False, member=False, **kwargs):
         """ Returns matching entries for search in ldap
-            structured as [(dn, {attributes})] 
+            structured as [(dn, {attributes})]
             UNLESS searching by dn, in which case the first match
             is returned
-        """ 
+        """
         scope = pyldap.SCOPE_SUBTREE
         if not base:
             base = self.users
-        
+
         filterstr =''
         for key, value in kwargs.iteritems():
             filterstr += '({0}={1})'.format(key,value)
-            if key == 'dn': 
+            if key == 'dn':
                 filterstr = '(objectClass=*)'
                 base = value
                 scope = pyldap.SCOPE_BASE
                 break
-        
+
         if len(kwargs) > 1:
             filterstr = '(&'+filterstr+')'
-        
+
         result = self.ldap.search_s(base, pyldap.SCOPE_SUBTREE, filterstr, ['*','+'])
         if base == self.users:
             for member in result:
@@ -123,10 +123,11 @@ class CSHLDAP:
                 if 'eboard' in member[1]['groups']:
                     member[1]['committee'] = self.search(base=self.committees, \
                            head=member[0])[0][1]['cn'][0]
+        if member:
+            return Member(result)
+        finalResult = self.trimResult(result) if trim else result
+        return finalResult
 
-        if trim: return self.trimResult(result)
-        else:    return result
-    
     def modify( self, uid, base=False, **kwargs ):
         if not base:
             base = self.users
@@ -138,5 +139,26 @@ class CSHLDAP:
             if field in old_attrs:
                 new_attrs[field] = [str(value)]
         modlist = pyldap.modlist.modifyModlist(old_attrs, new_attrs)
-        
+
         self.ldap.modify_s(dn, modlist)
+
+    class Member(object):
+        def __init__(self, searchResult):
+            if len(searchResult) == 0:
+                self.memberDict = {}
+                return
+            searchResultTuple = searchResult[0]
+            if len(searchResultTuple) < 2:
+                self.memberDict = {}
+                return
+            searchResultDictionary = searchResultTuple[1]
+            self.memberDict = searchResultDictionary
+
+        def __getattr__(self, attribute):
+            try:
+                attributes = self.memberDict[attribute]
+                if len(attributes) == 1:
+                    return attributes[0]
+                return attributes
+            except (KeyError, IndexError):
+                return None
